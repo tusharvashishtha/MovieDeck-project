8cd20779bb2bca6976fe6da2248e83ee0950d22d
Siddharth6511 <143956632+Siddharth6511@users.noreply.github.com>
2023-12-03 23:18:10 +0530

    movieDeck

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 const Apikey = `ec920dfb6377429c67744e902c758536`
 const api = `https://api.themoviedb.org/3/movie/top_rated?api_key=${Apikey}&language=en-US`
 const movieListingTag = document.querySelector('.moviesListing');
@@ -7,6 +8,7 @@ const prevBtnTag = document.querySelector('.prevbtn');
 const nextBtnTag = document.querySelector('.nextbtn');
 const sortByDatebtnTag = document.querySelector('.sortByDate');
 const sortByratingBtnTag = document.querySelector('.sortByRating');
+const searchBtnTag = document.querySelector('.searchBtn')
 let currentMovieData = '';
 let sortedByDateMovieData = '';
 let sortedByRatingMovieData = '';
@@ -15,6 +17,11 @@ let isSortedByRatings = false;
 
 
 // Assigning events to the buttons
+
+
+searchBtnTag.addEventListener('click', (e) => {
+    e.preventDefault();
+})
 
 prevBtnTag.addEventListener('click', () => {
     if(currentpage === 1){
@@ -73,31 +80,109 @@ sortByratingBtnTag.addEventListener('click', () => {
     updateMoviePage(sortedByRatingMovieData);
 
 })
+
+// function handleFilter(clicked, supplement, clickedDetaSet){
+//     if(isSortedByRatings){
+//         isSortedByRatings = false;
+//         isSortedByDate = false;
+//         updateMoviePage(currentMovieData)
+//         return;
+//     }  
+
+//     isSortedByRatings = true;
+//     isSortedByDate = false;
+//     if(!sortedByRatingMovieData){
+//         sortedByRatingMovieData = sortMovie([...currentMovieData] ,'rating');
+//     }
+//     updateMoviePage(sortedByRatingMovieData);
+
+// }
  
 
 
 function updateMoviePage(movieArray){
     // const updateMovieListing = document.createDocumentFragment();
     let updateMovieListing = '';
-    for(let {title, vote_count,vote_average,poster_path} of movieArray){
+    for(let {title, vote_count,vote_average,poster_path, isFavourite, id} of movieArray){
         const movieUrl = `${IMAGE_URL}/${poster_path}`
         const movieCard = `<div class="movie-card">
         <img src = "${movieUrl}" alt="">
-        <div>
-            <div class="movie-details">
+
+             <div class="movie-details">
                 <h5>${title}</h5>
                 <div>
                     <span>Votes: ${vote_count}</span>
                     <span>Ratings: ${vote_average}</span>
                 </div>
-            </div>
-            <span>icon</span>
+                <i 
+                class="fa-heart ${isFavourite ? 'fa-solid' : 'fa-regular'}"
+                style="color: #ff2e2e;" 
+                id = "${id}"
+                data-isfavourite = "${isFavourite}"
+                >
+                </i>
+
         </div>
     </div>`
     updateMovieListing += movieCard;
     }
     movieListingTag.innerHTML = updateMovieListing;
     //   movieListingTag.innerHTML = '';
+
+  const favIframeTags =  document.querySelectorAll('.fa-heart');
+  for(let favIcon of favIframeTags){
+    favIcon.addEventListener('click', handleFavourite)
+  }
+}
+
+function handleFavourite(e){
+    let isRemove = false;
+    if(e.target.dataset.isFavourite  == 'true'){
+        e.target.classList.remove('fa-solid')
+        e.target.classList.add('fa-regular')
+        e.target.dataset.isFavourite = false; 
+        isRemove = true;       
+    }
+    else{
+        e.target.classList.remove('fa-regular')
+        e.target.classList.add('fa-solid')
+        e.target.dataset.isFavourite = true;
+    }
+
+   const filteredArr = currentMovieData.filter((val, index) => {
+    return val.id == e.target.id;
+    })
+
+   const localMovieStorage = JSON.parse(localStorage.getItem('favouriteMoiveList'));
+
+
+   if(!isRemove){
+    let newFavObj;
+
+   if(localMovieStorage){
+    newFavObj = {
+        [filteredArr[0].id] : filteredArr[0],
+        ...localMovieStorage
+      } 
+   }
+   
+   else{
+       newFavObj = {
+           [filteredArr[0].id] : filteredArr[0]
+        } 
+    }
+     localStorage.setItem('favouriteMoiveList', JSON.stringify(newFavObj));
+   }
+     else{
+        if(localMovieStorage){
+            delete localMovieStorage[filteredArr[0].id]
+        }
+        localStorage.setItem('favouriteMoiveList', JSON.stringify(localMovieStorage))
+   }
+    
+   
+
+    
 }
 async function getPaginatedMovieData(page = 1){
     resetPageValues()
@@ -126,17 +211,18 @@ function sortMovie(movieArray, sortBy){
             movieObjB.epochTime = new Date(movieObjB[sortingKey]);
             return movieObjA.epochTime - movieObjB.epochTime
         })
-        return movieArray;
     }
 
     else if(sortBy === 'rating'){
         sortingKey = 'vote_average';
+        movieArray.sort((movieObjA, movieObjB) => {
+            return movieObjA[sortingKey] - movieObjB[sortingKey];
+        })
     }
 
-    movieArray.sort((movieObjA, movieObjB) => {
-        return movieObjA[sortingKey] - movieObjB[sortingKey];
-    })
     return movieArray;
 }
 
         getPaginatedMovieData(currentpage);
+
+       
